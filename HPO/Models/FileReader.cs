ad3b9b53615f7ec9379e9dac803bb073322a0b63
MarcelFedorV <fedorovicimarcel@gmail.com>
2025-03-25 18:49:17 +0200

    ListReader-Start
    Worked with Serafeim on reading the csv file and turning it into a list. We're at the starting point with this

---
+++ HPO/Models/FileReader.cs
@@ -0,0 +1,34 @@
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System;
+
+namespace HPO.Models;
+
+public class FileReader
+{
+public void ReadInfo()
+    {
+        List<Winter> winterList = new();
+        using (var reader = new StreamReader("HPOInfo.csv"))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            winterList = csv.GetRecords<Winter>().ToList();
+        }
+
+
+
+        foreach (var winter in winterList)
+        {
+
+            Console.WriteLine(winter.TimeFrom);
+            System.Diagnostics.Debug.WriteLine(winter.TimeFrom);
+
+
+        }
+    }
+}
--- HPO/Models/Info.cs
+++ HPO/Models/Info.cs
@@ -1,5 +1,48 @@
 namespace HPO.Models;
 
+public abstract class Information
+{
+    public string? TimeFrom { get; set;}
+
+    public string? TimeTo { get; set;}
+
+    public double? HeatDemand { get; set;}
+
+    public double? HeatPrice { get; set;}
+
+    public Information(string? timeFrom, string? timeTo, double? heatDemand, double? heatPrice)
+    {
+        TimeFrom = timeFrom;
+        TimeTo = timeTo;
+        HeatDemand = heatDemand;
+        HeatPrice = heatPrice;
+    }
+} 
+
+public class Winter : Information
+{
+    public Winter(string? timeFrom, string? timeTo, double? heatDemand, double? heatPrice)
+            : base(timeFrom, timeTo, heatDemand, heatPrice) { }
+}
+
+public class Summer : Information
+{
+    public Summer(string? timeFrom, string? timeTo, double? heatDemand, double? heatPrice)
+            : base(timeFrom, timeTo, heatDemand, heatPrice) { }
+}
+
+public class Starter
+{
+public void MakinFunction()
+{
+    Winter winter = new Winter("23/07/2024","27/08/2024",23.2,524.56);
+    Summer summer = new Summer("25/08/2024","29/09/2024",35.2,213.56);
+}
+
+}
+
+
+/*
 public class Info
 {
     public string? WinterTimeFrom { get; set; }
@@ -25,3 +68,5 @@ public class Info
 
 
 }
+
+*/
