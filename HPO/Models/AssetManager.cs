c98dd0b125919533013285f09ceb4976497f542f
Bartosh2005 <bartorski2005@o2.pl>
2025-03-26 11:33:21 +0100

    further work on gui and graphs

--- HPO/Models/AssetManager.cs
+++ HPO/Models/AssetManager.cs
@@ -3,27 +3,26 @@ using System.Collections.ObjectModel;
 
 namespace HPO.Models;
 
-public class Boiler
+public abstract class Boiler
 {
     public string Name { get; set; }
     public float Heat { get; set; }
     public float ProdCost { get; set; }
     public float CO2Emission { get; set; }
-    public float GasConsumption { get; set; }
-    public float OilConsumption { get; set; }
+    public float Consumption { get; set; }
 
-    public Boiler(string name, float maxHeat, float prodCost, float co2Emission, float gasConsumption)
+    public Boiler(string name, float maxHeat, float prodCost, float co2Emission, float consumption)
     {
         Name = name;
         Heat = maxHeat;
         ProdCost = prodCost;
         CO2Emission = co2Emission;
-        GasConsumption = gasConsumption;
+        Consumption = consumption;
     }
 }
 public class GasBoiler : Boiler
 {
-    public GasBoiler(string name, float heat, float prodCost, float co2Emission, float gasConsumption) : base(name, heat, prodCost, co2Emission, gasConsumption)
+    public GasBoiler(string name, float heat, float prodCost, float co2Emission, float consumption) : base(name, heat, prodCost, co2Emission, consumption)
     {
 
     }
@@ -31,7 +30,7 @@ public class GasBoiler : Boiler
 
 public class OilBoiler : Boiler
 {
-    public OilBoiler(string name, float heat, float prodCost, float co2Emission, float oilConsumption) : base(name, heat, prodCost, co2Emission, oilConsumption)
+    public OilBoiler(string name, float heat, float prodCost, float co2Emission, float consumption) : base(name, heat, prodCost, co2Emission, consumption)
     {
     }
 }
