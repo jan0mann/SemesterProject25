079d223fce4c631bbba6e190ed1a8f2d10f0e93c
Mikolaj Ostrowski <mikolaj.p.ostrowski@gmail.com>
2025-04-02 15:32:29 +0200

    automatic .csv in /bin (.csproj) changed names for more self explanatory (AssetManager.cs & Info.cs) combined all boilers into one list (BoilerInfoViewModel.cs) Boiler Info Page more visually appealing

--- HPO/Models/Manager.cs
+++ HPO/Models/Manager.cs
@@ -4,24 +4,18 @@ namespace HPO.Models
 {
     public class AssetManager
     {
-        public (ObservableCollection<GasBoiler> GasBoilers, ObservableCollection<OilBoiler> OilBoilers) ShowInfo()
+
+        public ObservableCollection<Boiler> ShowInfoCombined()//change the name to just ShowInfo after removing the old method
         {
-            var gasBoilers = new ObservableCollection<GasBoiler>
+            var Boilers = new ObservableCollection<Boiler>
             {
                 new GasBoiler("Gas Boiler 1", 4.0f, 520f, 175f, 0.9f),
                 new GasBoiler("Gas Boiler 2", 3.0f, 560f, 130f, 0.7f),
-
-            };
-
-            var oilBoilers = new ObservableCollection<OilBoiler>
-            {
                 new OilBoiler("Oil Boiler 1", 4.0f, 670f, 330f, 1.5f)
-
             };
 
-            return (gasBoilers, oilBoilers);
+            return Boilers;
         }
-
 
     }
 }
--- HPO/ViewModels/BoilerInfoViewModel.cs
+++ HPO/ViewModels/BoilerInfoViewModel.cs
@@ -6,34 +6,24 @@ namespace HPO.ViewModels
 {
     public partial class BoilerInfoViewModel : ViewModelBase
     {
-        private ObservableCollection<GasBoiler> _gasBoilers;
-        private ObservableCollection<OilBoiler> _oilBoilers;
+        private ObservableCollection<Boiler> _Boilers;
 
-        public ObservableCollection<GasBoiler> GasBoilers
+        public ObservableCollection<Boiler> Boilers
         {
-            get { return _gasBoilers; }
-            set { SetProperty(ref _gasBoilers, value); }
-        }
-
-        public ObservableCollection<OilBoiler> OilBoilers
-        {
-            get { return _oilBoilers; }
-            set { SetProperty(ref _oilBoilers, value); }
+            get { return _Boilers; }
+            set { SetProperty(ref _Boilers, value); }
         }
 
         public BoilerInfoViewModel()
         {
-            GasBoilers = new ObservableCollection<GasBoiler>();
-            OilBoilers = new ObservableCollection<OilBoiler>();
+            Boilers = new ObservableCollection<Boiler>();
             InitializeBoilers();
         }
 
         private void InitializeBoilers()
         {
             var assetManager = new AssetManager();
-            var (gasBoilers, oilBoilers) = assetManager.ShowInfo();
-            GasBoilers = gasBoilers;
-            OilBoilers = oilBoilers;
+            Boilers = assetManager.ShowInfoCombined();
         }
     }
 }
