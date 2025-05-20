077f99a114d2883f643031bf2b1e94f0b24eabb0
jan0mann <jan.ferchof@icloud.com>
2025-05-20 14:47:10 +0200

    Reapply "Merge branch 'master' of https://github.com/jan0mann/SemesterProject25"
    This reverts commit 884312c67a8917c96abfb253eff49a5aebc05e7c.

--- HPO/Models/Manager.cs
+++ HPO/Models/Manager.cs
@@ -5,18 +5,25 @@ namespace HPO.Models
     public class AssetManager
     {
 
-        public ObservableCollection<Boiler> ShowInfo()
+        public ObservableCollection<Boiler> ShowInfo2()
         {
             var Boilers = new ObservableCollection<Boiler>
             {
-                // new GasBoiler("Gas Boiler 1", 4.0f, 1520f, 175f, 0.9f),
-                // new GasBoiler("Gas Boiler 2", 3.0f, 1560f, 130f, 0.7f),
-                // new OilBoiler("Oil Boiler 1", 4.0f, 670f, 330f, 1.5f)
                 new Boiler("Gas Boiler 1", BoilerType.Gas, 4.0f, 520f, 175f, 0.9f, 0),
-                // new Boiler("Gas Boiler 2", BoilerType.Gas, 3.0f, 560f, 130f, 0.7f),
                 new Boiler("Oil Boiler 1", BoilerType.Oil, 4.0f, 670f, 330f, 1.5f, 0),
                 new Boiler("Gas Motor 1", BoilerType.GasMotor, 3.5f, 990f, 650f, 1.8f, 2.6),
                 new Boiler("Heat Pump 1", BoilerType.HeatPump, 6.0f, 60f, 0f, 0f, -6.0)
+            };
+
+            return Boilers;
+        }
+        public ObservableCollection<Boiler> ShowInfo1()
+        {
+            var Boilers = new ObservableCollection<Boiler>
+            {
+                new Boiler("Gas Boiler 1", BoilerType.Gas, 4.0f, 520f, 175f, 0.9f, 0),
+                new Boiler("Gas Boiler 2", BoilerType.Gas, 3.0f, 560f, 130f, 0.7f, 0),
+                new Boiler("Oil Boiler 1", BoilerType.Oil, 4.0f, 670f, 330f, 1.5f, 0)
             };
 
             return Boilers;
