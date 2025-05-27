ef2dafa361f3045f5c51bd9b93439033a79ed378
Mikolaj Ostrowski <mikolaj.p.ostrowski@gmail.com>
2025-05-27 16:43:04 +0200

    not finished

--- HPO/ViewModels/FirstViewModel.cs
+++ HPO/ViewModels/FirstViewModel.cs
@@ -1,5 +1,10 @@
 using System;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using LiveChartsCore.SkiaSharpView.Avalonia;
+using LiveChartsCore.SkiaSharpView.SKCharts;
 
 namespace HPO.ViewModels;
 
--- HPO/Views/FirstView.axaml.cs
+++ HPO/Views/FirstView.axaml.cs
@@ -2,6 +2,14 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using HPO.ViewModels;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using LiveChartsCore.SkiaSharpView.Avalonia;
+using LiveChartsCore.SkiaSharpView.SKCharts;
+using System;
+using System.IO;
+using CommunityToolkit.Mvvm.ComponentModel;
+
 
 namespace HPO.Views;
 
@@ -11,6 +19,27 @@ public partial class FirstView : UserControl
     {
         InitializeComponent();
         DataContext = new HeatDemandViewModel();
+        //CreateImageFromCartesianControl();
+    }
+
+
+    public void CreateImageFromCartesianControl(int scenario)
+    {
+        var HeatDemandViewModel = new HeatDemandViewModel();//as for both winter and summer the number of current scenatio is set at the same time, thus it doesnt matter which value we are using
+        string name="";
+        // if (scenario == 'w')
+        //     name = "winterChartScenario";
+        // else
+        //     name = "summerChartScenario";
+        //string fullName = name + HeatDemandViewModel.CurrentSummerScenario.ToString() + ".png";
+        string fullName = name + scenario + ".png";
+
+        string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads", fullName);
+        //var chartControl = this.FindControl<CartesianChart>(scenario=='w'?"winterMainChart":"summerMainChart");
+        var chartControl = this.FindControl<CartesianChart>("winterMainChart");
+        var skChart = new SKCartesianChart(chartControl) { Width = 1920, Height = 1080, };
+        skChart.SaveImage(path);
+        Console.WriteLine($"Image saved succesfully @ {path}");
     }
 
     private void InitializeComponent()
